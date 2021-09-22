a22c2b2d4d81dd93c9305964d59c536609b948d7
Tom Cool <tom.cool@indivirtual.com>
2021-09-23 00:12:44 +0200

    added int32ToIpv4

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,19 +1,58 @@
 
-import { ipToSeries, ipToInt32, ipsBetween } from './';
+import { throwInvalidIpv4, ipv4ToOctets, ipv4ToInt32, ipsBetween, int32ToIpv4 } from './';
 
-test('Simply convert an IP to an array of numbers with `ipToSeries`', () => {
-    expect(ipToSeries('10.0.0.0')).toStrictEqual([10, 0, 0, 0])
+
+describe('throwInvalidIpv4:', () => {
+
+    it('throws an error when the input ipv4 is empty', () => {
+        expect(() => throwInvalidIpv4('')).toThrow('UNDEFINED_IP')
+    })
+
+    it('throws an error when the input ipv4 is invalid', () => {
+        expect(() => throwInvalidIpv4('1.2.3')).toThrow('INVALID_IP')
+        expect(() => throwInvalidIpv4('5.4.3.2.1')).toThrow('INVALID_IP')
+        expect(() => throwInvalidIpv4('22')).toThrow('INVALID_IP')
+    })
+
+    it('does not throw when th ipv4 is valid', () => {
+        expect(() => throwInvalidIpv4('192.168.0.1')).not.toThrow()
+    })
 })
 
-test('Convert an IP to a decimal with `ipToInt32`', () => {
-  expect(ipToInt32('192.168.0.1')).toStrictEqual(3232235521)
-  expect(ipToInt32('10.0.0.50')).toStrictEqual(167772210)
-  expect(ipToInt32('10.0.0.0')).toStrictEqual(167772160)
+describe('ipv4ToOctets: ', () => {
+
+    it('simply converts an IP to an array of 4 numbers', () => {
+        expect(ipv4ToOctets('10.0.0.0')).toStrictEqual([10, 0, 0, 0])
+    })
 })
 
-test('Calculate the number of IP adresses between 2 IP`s with `ipsBetween`', () => {
-  expect(ipsBetween('10.0.0.0', '10.0.0.50')).toStrictEqual(50)
-  expect(ipsBetween('10.0.0.50', '10.0.0.0')).toStrictEqual(50)
-  expect(ipsBetween('20.0.0.10', '20.0.1.0')).toStrictEqual(246)
-  expect(ipsBetween('20.0.1.0', '20.0.0.10')).toStrictEqual(246)
+describe('ipv4ToInt32:', () => {
+    
+    it('converts an IP to a decimal', () => {
+        expect(ipv4ToInt32('192.168.0.1')).toBe(3232235521)
+        expect(ipv4ToInt32('10.0.0.50')).toBe(167772210)
+        expect(ipv4ToInt32('10.0.0.0')).toBe(167772160)
+    })
+})
+
+describe('ipsBetween:', () => {
+
+    it('calculates the number of IP adresses between 2 IP\'s', () => {
+        expect(ipsBetween('10.0.0.0', '10.0.0.50')).toBe(50)
+        expect(ipsBetween('20.0.1.0', '20.0.0.10')).toBe(246)
+    })
+
+    it('always returns a positive number', () => {
+        expect(ipsBetween('10.0.0.0', '10.0.0.50')).toBeGreaterThan(0)
+        expect(ipsBetween('20.0.0.10', '20.0.1.0')).toBeGreaterThan(0)
+    })
+})
+
+describe('int32ToIpv4:', () => {
+
+    it('generates a valid ipv4 from a number', () => {
+        expect(int32ToIpv4(2149583361)).toBe('128.32.10.1')
+        expect(int32ToIpv4(32)).toBe('0.0.0.32')
+        expect(int32ToIpv4(0)).toBe('0.0.0.0')
+    })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,44 @@
 
 import { IpAsArray } from './model'
 
+
+const ipv4RexExp: RegExp = new RegExp('^([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})$');
+
+export function throwInvalidIpv4(ip: string): void {
+
+    if (!ip) {
+        throw new Error('UNDEFINED_IP')
+    }
+
+    if (!ipv4RexExp.test(ip)) {
+        throw new Error('INVALID_IP')
+    }
+}
+
 /**
  * Takes a IP as string, and rturns a series with 4 numbers
  * @param ip IP as string
  * @returns An array containing 4 numbers
  * @example '10.0.0.50' = [10, 0, 0, 50]
  */
-export function ipToSeries(ip: string): IpAsArray {
-    return ip.split('.').map(n => +n) as IpAsArray
+export function ipv4ToOctets(ip: string): IpAsArray {
+
+    throwInvalidIpv4(ip)
+
+    const result = ip.split('.').map(n => +n)
+
+    return result as IpAsArray
 }
 
 /**
- * Takes a IP as string and converts it to 
+ * Takes a IP as string and converts it to
  * @param ip A string, like '192.168.0.1'
  * @returns number
  * @example 192.168.0.1 = 1 + (0 * 256) + (168 * 256 * 256) + (192 * 256 * 256 * 256) = 3232235521
  */
-export function ipToInt32(ip: string): number {
-    return ipToSeries(ip).reduce((r, i) => (r * 256) + +i)
+export function ipv4ToInt32(ip: string): number {
+    throwInvalidIpv4(ip)
+    return ipv4ToOctets(ip).reduce((ipInt, octet) => (ipInt * 256) + +octet)
 }
 
 /**
@@ -29,7 +49,15 @@ export function ipToInt32(ip: string): number {
  * @example ipsBetween("10.0.0.0", "10.0.0.50") = 50
  */
 export function ipsBetween(ip1: string, ip2: string): number {
-    return Math.abs(ipToInt32(ip1) - ipToInt32(ip2))
+    return Math.abs(ipv4ToInt32(ip1) - ipv4ToInt32(ip2))
 }
 
-// takes in a 32-bit unsigned integer and returns its equivalent ipv4 address
+/**
+ * Takes in a 32-bit unsigned integer and returns its equivalent ipv4 address
+ * @param int32
+ * @returns
+ */
+export function int32ToIpv4(int32: number) {
+    return [int32 >>> 24 & 0xFF, int32 >>> 16 & 0xFF, int32 >>> 8 & 0xFF, int32 & 0xFF].join('.')
+};
+
